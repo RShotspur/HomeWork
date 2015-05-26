05e5f2c4beb2cb9294d7bd4c441421b338b7533e
RShotspur <rs.hotspur@gmail.com>
2015-05-26 17:35:24 +0300

    HomeWork done

--- src/codes/lesson1.java
+++ src/codes/lesson1.java
@@ -4,5 +4,29 @@ package codes;
  * Created by roman.sydorenko on 25/05/2015.
  */
 public class lesson1 {
+    public static void main(String[] args) {
+        System.out.println("Hello World");
 
+            byte byteNumber = 127;
+            short shortNumber = 256;
+            int intNumber = 1000;
+            long longNumber = 1235447882;
+
+            char charSymbol = 'g';
+
+            float floatNumber = 10.5f;
+            double doubleNumber = 10.5d;
+
+            boolean booleanValue = true;
+
+        System.out.println(byteNumber);
+        System.out.println(shortNumber);
+        System.out.println(intNumber);
+        System.out.println(longNumber);
+        System.out.println(charSymbol);
+        System.out.println(floatNumber);
+        System.out.println(doubleNumber);
+        System.out.println(booleanValue);
+
+    }
 }
