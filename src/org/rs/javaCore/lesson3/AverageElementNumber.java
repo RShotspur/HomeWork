87c4db4cdf72fa111ed48cd9ed0b649970b2df93
RS <rs.hotspur@gmail.com>
2015-06-02 21:26:13 +0300

    task 1-2-3-4 done

--- src/org/rs/javaCore/lesson3/AverageElementNumber.java
+++ src/org/rs/javaCore/lesson3/AverageElementNumber.java
@@ -5,6 +5,25 @@ package org.rs.javaCore.lesson3;
  */
 public class AverageElementNumber {
     public static void main(String[] args) {
+        int[] massive = Main.CreateRandMassive(Main.Rand());
+        int sum = 0;
+        for (int i = 0; i < massive.length; i++) {
+            sum += massive[i];
+        }
+        double avarage = (double) sum/(double)massive.length;
+        System.out.println(avarage);
+        for (int i = 0; i < massive.length; i++) {
+            System.out.print(massive[i] + " ");
+        }
+        double avarageNumber = avarage*100;
+        System.out.println();
+        for (int i = 0; i < massive.length; i++) {
 
+            if ((Math.abs((double)massive[i]-avarage)) < Math.abs(avarageNumber-avarage)) {
+                avarageNumber = massive[i];
+            }
+        }
+        System.out.println();
+        System.out.println((int)avarageNumber);
     }
 }
