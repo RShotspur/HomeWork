7e84ac2108b63b4bad7d46d7f0ad59d38bb51d5d
RS <rs.hotspur@gmail.com>
2015-06-02 19:57:00 +0300

    Task1 done

--- src/org/rs/javaCore/lesson3/AverageElementNumber.java
+++ src/org/rs/javaCore/lesson3/AverageElementNumber.java
@@ -5,6 +5,25 @@ package org.rs.javaCore.lesson3;
  */
 public class AverageElementNumber {
     public static void main(String[] args) {
-        System.out.println(Main.Rand());
+        int[] massive = new int[10];
+        massive[0] = -1;
+        massive[1] = 2;
+        massive[2] = -5;
+        massive[3] = 6;
+        massive[4] = -3;
+        massive[5] = 5;
+        massive[6] = -8;
+        massive[7] = 7;
+        massive[8] = 9;
+        massive[9] = -222;
+
+        for (int i = 0; i < massive.length; i++) {
+            if (massive[i] < 0) {
+                massive[i] *= -1;
+            }
+        }
+        for (int i = 0; i < massive.length; i++) {
+            System.out.println(massive[i]);
+        }
     }
 }
