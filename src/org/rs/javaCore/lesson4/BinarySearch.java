29fd8e595f6d1e855bb713aed7c91760fa0c0225
RS <rs.hotspur@gmail.com>
2015-06-09 20:55:56 +0300

    lesson 4 task 1,3 done

---
+++ src/org/rs/javaCore/lesson4/BinarySearch.java
@@ -0,0 +1,13 @@
+package org.rs.javaCore.lesson4;
+
+/**
+ * Created by ADI on 09.06.2015.
+ */
+public class BinarySearch {
+    //TODO should be done (((( but hadn't
+    public static void main(String[] args) {
+        int[] mas = SortMas.Sort(Main.CreateRandMassive(Main.Rand()));
+
+
+    }
+}
--- src/org/rs/javaCore/lesson4/SortMas.java
+++ src/org/rs/javaCore/lesson4/SortMas.java
@@ -1,25 +1,29 @@
 package org.rs.javaCore.lesson4;
 
+import sun.misc.Sort;
+
 /**
  * Created by ADI on 09.06.2015.
  */
 public class SortMas {
     public static void main(String[] args) {
-        int[] mas = Main.CreateRandMassive(Main.Rand());
-        int temp;
-        //Sort array like that
-        for (int i = 0; i < mas.length; i++) {
-            for (int j = i; j < mas.length; j++) {
-                if (mas[j] < mas[i]){
-                    temp = mas[i];
-                    mas[i] = mas[j];
-                    mas[j] = temp;
-                }
-            }
-        }
+        int[] mas = Sort(Main.CreateRandMassive(Main.Rand()));
         for (int i = 0; i < mas.length; i++) {
             System.out.println(mas[i]);
         }
+    }
+    public static int[] Sort(int[] input){
+        int temp;
+        for (int i = 0; i < input.length; i++) {
+            for (int j = i; j < input.length; j++) {
+                if (input[j] < input[i]){
+                    temp = input[i];
+                    input[i] = input[j];
+                    input[j] = temp;
+                }
+            }
+        }
+        return input;
     }
 
 
