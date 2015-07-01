f4f7b2f6905bcd28c20bc94087858351634d0d2f
RS <rs.hotspur@gmail.com>
2015-07-01 09:08:31 +0300

    lesson 10 - 2 beginning

---
+++ src/org/rs/javaCore/lesson10/Main.java
@@ -0,0 +1,10 @@
+package org.rs.javaCore.lesson10;
+
+/**
+ * Created by ADI on 28.06.2015.
+ */
+public class Main {
+    public static void main(String[] args) {
+
+    }
+}
---
+++ src/org/rs/javaCore/lesson10/SquareEquation.java
@@ -0,0 +1,10 @@
+package org.rs.javaCore.lesson10;
+
+/**
+ * Created by ADI on 01.07.2015.
+ */
+public class SquareEquation {
+    private int x;
+    private int y;
+    private int z;
+}
